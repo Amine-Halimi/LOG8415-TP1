5d0dc8827be4ab9e937f30d1ad5605e36deccfa8
Amine.H <kilner99@gmail.com>
2024-09-20 21:03:30 -0400

    Feat: Creating the file scriptToRunTheInstances.py

---
+++ scriptToRunTheInstances.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+
+subprocess.run(["C:\Programming_Languages\Python\Python3\Python_3.12.5\python.exe", ".\\benchmarkscript.py"])
+"""
+#creates instances and target groups
+def create_instances():
+    os("")
+
+#deletes instances and target groups
+def delete_instances():
+    os("")
+
+#load all the instances with the main.py and the right environment. Also gets all the application running.
+def loading_instances():
+    os("")
+
+#create the application load balancer with the two target groups.
+def create_application_load_balancer():
+    os("")
+
+#deletes the application load balancer with the two target groups.
+def delete_application_load_balancer():
+    os("")
+
+#Runs benchmarkscript.py
+def run_benchmarkscript():
+    os("python3 .\\benchmarscript.py")
+
+if __name__ == " __main__ ":
+    subprocess.run("ls", "-l")
+"""
+
+        
